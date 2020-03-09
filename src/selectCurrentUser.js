80d6715103e95c912ff15961789cc1bde9ebb209
Eledoux <lawanledoux@gmail.com>
2020-03-09 18:13:29 +0100

    selectCurrentUser working with state.data.users not defined

--- src/selectCurrentUser.js
+++ src/selectCurrentUser.js
@@ -2,10 +2,10 @@ import { createSelector } from 'reselect'
 
 import currentUserUUID from './currentUserUUID'
 
-export const selectCurrentUser = createSelector(
-  state => state.data.users,
-  users => users.find(user => user &&
-    user.currentUserUUID === currentUserUUID)
-)
 
-export default selectCurrentUser
+export default createSelector(
+  state => state.data.users,
+  users => {
+    if (!users) return
+    users.find(user => user && user.currentUserUUID === currentUserUUID)
+  })
